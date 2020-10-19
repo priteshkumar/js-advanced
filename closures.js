c8dad04ba351abbc2ec4bbfe26e84542dc663a28
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-19 21:16:04 +0530

    add more closure example

--- closures.js
+++ closures.js
@@ -1,4 +1,5 @@
 //closure demo 
+//closure is used to create private methods/variables
 
 function mathsFunc(op1,op2,operator){
     var calculate;
@@ -26,3 +27,16 @@ function mathsFunc(op1,op2,operator){
 //forms a closure
 var calc = mathsFunc(2,3,"+");
 console.log(calc());
+
+
+//closure code example
+function printStats(msg){
+    var timestamp  = new Date().toString();
+    var printFunc = function(){
+        console.log(timestamp + ' ' + msg);
+    }
+    return printFunc;
+}
+
+var f = printStats('jenkins build started');
+f();
