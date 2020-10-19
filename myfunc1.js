86dbca3b3cedf6c7a342498a941aecbf3bf20c63
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-19 19:45:50 +0530

    add function hoisting demo

--- myfunc1.js
+++ myfunc1.js
@@ -52,3 +52,17 @@ var resMap = (function(){
 })();
 
 console.log(resMap);
+
+console.log(fibonacci(10));
+//recursive func
+function fibonacci(n){
+    if(n == 0)
+        return 1;
+    else if(n == 1)
+        return 1;
+    else
+        return fibonacci(n-1) + fibonacci(n-2);
+
+}
+
+console.log(fibonacci(10));
