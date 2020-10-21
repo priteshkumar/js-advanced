805ad1b3b74927ed122e0af209410d6d0cc06780
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-10-21 18:58:30 +0530

    rename es6 files

---
+++ es6demo.js
@@ -0,0 +1,38 @@
+//use of let/const 
+//enables block scoping of variables
+//hoisting is not done for let
+let val = 20 + 30;
+const snapshot = "latest";
+
+
+//arrow functions demo
+//function with a single return statement as below
+printfunc = () => "jenkins log";
+console.log(printfunc());
+
+let b = [10,20,30];
+//arrow function in map
+let c = b.map(x => x * x);
+
+
+//spread operator
+let d = [10,20];
+
+//concats [30] and [10,20] into e
+let e = [30,...d];
+console.log(e);
+
+//copies e into f using spread (...) operator
+let f = [...e];
+console.log(f);
+
+//string template
+//use backticks as below
+let version = 'latest';
+let citool = `jenkins-${version}`;
+console.log(citool);
+
+
+//more arrow functions
+let myArrowFunction = (a,b) => console.log(a + " Arrow function " + b);
+myArrowFunction("jenkins","docker");
